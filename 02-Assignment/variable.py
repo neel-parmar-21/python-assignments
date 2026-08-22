4dd34a296238e763707c4cb430d5e3178975741a
neel-parmar-21 <neel.parmar.cg@gmail.com>
2026-08-22 16:18:29 +0530

    Completed Assignment 1 and 2

---
+++ 02-Assignment/variable.py
@@ -0,0 +1,37 @@
+#Question 24
+name="Neel"
+age=17
+city="Pali"
+print(name,age,city)
+
+#Question 25
+student_name="Ansh"
+student_roll_no="21"
+student_branch="Computer Science"
+print(student_name,student_roll_no,student_branch)
+
+#Question 26
+marks=450
+marks=495
+print(marks)
+
+#Question 27
+name,age,city="Rahul",18,"Patna"
+print(name,age,city)
+
+#Question 28
+x=y=z=0
+print(x,y,z)
+
+#Question 29
+student1="Aryan"
+student_name="Rahul"
+student_class="B.Tech"
+print(student1,student_name,student_class)
+
+#Question 30
+#Student Information
+student_name="Jitesh"
+student_age=19
+student_age=20
+print(student_name,student_age)
