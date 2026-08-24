8f45f6a3ad556ed3146885db197047e9b5c69345
neel-parmar-21 <neel.parmar.cg@gmail.com>
2026-08-24 17:59:27 +0530

    Modified 03 Assignment

--- 03-Assignment/data-type.py
+++ 03-Assignment/data-type.py
@@ -5,31 +5,24 @@ student_height=5.11
 is_student=True
 student_result=None
 
-print(type(student_name))
-print(type(student_age))
-print(type(student_height))
-print(type(is_student))
-print(type(student_result))
+print(type(student_name),type(student_age),type(student_height),
+      type(is_student),type(student_result))
 
 #Q25
 a=50
 b=50.0
 c="50"
-print(type(a))
-print(type(b))
-print(type(c))
+print(type(a),type(b),type(c))
 
 #Q26
 a=True
 b="True"
-print(type(a))
-print(type(b))
+print(type(a),type(b))
 
 #Q27
 a=None
 b="None"
-print(type(a))
-print(type(b))
+print(type(a),type(b))
 
 #Q28
 value=21
@@ -44,11 +37,8 @@ product_quantity=10
 product_price=99999.99
 product_availability=False
 product_discount_info=None
-print(type(product_name))
-print(type(product_quantity))
-print(type(product_price))
-print(type(product_availability))
-print(type(product_discount_info))
+print(type(product_name),type(product_quantity),type(product_price),
+      type(product_availability),type(product_discount_info))
 
 #30
 a=10
@@ -58,10 +48,6 @@ d=True
 e="True"
 f=None
 g="None"
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
-print(type(g))
+print(type(a),type(b),type(c),
+      type(d),type(e),type(f),
+      type(g))
