ba02276c4697c33a2242346cfc15fc102e6144e9
neel-parmar-21 <neel.parmar.cg@gmail.com>
2026-08-25 23:05:19 +0530

    Complete 4th Assignment

---
+++ 04-Assignment/operators.py
@@ -0,0 +1,196 @@
+#Task 1
+num1=5
+num2=6
+print("Addition: ",num1+num2) #Addition
+print("Subtraction: ",num1-num2) #Subtraction
+print("Multiplication: ",num1*num2) #Multiplication
+print("Division: ",num1/num2) #Division
+print("Floor Division: ",num1//num2) #Fllor Division
+print("Modulus: ",num1%num2) #Modulus
+print("Exponentiation: ",num1**num2) #Exponentiation
+
+#Task 2
+int=2
+float=3.5
+print("Addition: ",int+float) #Addition
+print("Subtraction: ",int-float) #Subtraction
+print("Multiplication: ",int*float) #Multiplication
+print("Division: ",int/float) #Division
+print("Floor Division: ",int//float) #Fllor Division
+print("Modulus: ",int%float) #Modulus
+print("Exponentiation: ",int**float) #Exponentiation
+
+#Task 3
+english_marks=83
+science_marks=89
+maths_marks=75
+total_marks=(english_marks+science_marks+maths_marks)
+average_marks=((english_marks+science_marks+maths_marks)/3)
+print("Total marks: ",total_marks)
+print("Average marks: ",average_marks)
+
+#Task 4
+product_price=1200
+quantity=30
+total_price=product_price*quantity
+print("Total price: ",total_price)
+
+#Task 5
+num=20
+if num%2==0:
+   print("The number is even")
+else:
+   print("The number is odd")
+
+#Task 6
+number1=10
+number2=20
+number3=-30
+print("Normal Division: ",number1/number2)
+print("Floor Division: ",number1//number2)
+print("Normal Division: ",number1/number3)
+print("Floor Division: ",number1//number3)
+
+#Task 7
+neg_num1=-3
+neg_num2=-4
+print("Addition: ",neg_num1+neg_num2)
+print("Subtraction: ", neg_num1-neg_num2)
+print("Multiplication: ", neg_num1*neg_num2)
+print("Division: ", neg_num1/neg_num2)
+print("Floor Division: ", neg_num1//neg_num2)
+print("Modulus: ", neg_num1//neg_num2)
+
+#Task 8
+num3=2
+num4=4
+num5=-2
+num6=-9
+print("positive - positive: ",num3-num4)
+print("positive - negative: ",num3-num5)
+print("negative - positive: ",num5-num3)
+print("negative - negative: ",num5-num6)
+
+#Task 9
+print("positive // positive: ",num3//num4)
+print("positive // negative: ",num3//num5)
+print("negative // positive: ",num5//num3)
+print("negative // negative: ",num5//num6)
+
+#Task 10
+print("positive % positive: ",num3%num4)
+print("positive % negative: ",num3%num5)
+print("negative % positive: ",num5%num3)
+print("negative % negative: ",num5%num6)
+
+#Task 11
+print(10 + 5 * 2) # *>+
+print(20 - 4 / 2) # />-
+print(10 + 20 / 5 * 2) # />+
+print(2 + 3 * 4 ** 2) #**>*>+
+print(100 - 20 // 5) # //>-
+
+#Task 12
+print(10+5*2)
+print((10+5)*2)
+
+print(20-10/2)
+print((20-10)/2)
+
+print(2+3*4)
+print((2+3)*4)
+
+#Task 13
+true=True
+false=False
+print("Addition: ",true+false,type(true+false))
+print("Subtraction: ",true-false,type(true-false))
+print("Multiplication: ",true*false,type(true*false))
+# print("Division: ",true/false,type(true/false)) Shows error
+#print("Floor Division: ",true//false,type(true//false)) Shows error
+#print("Modulus:",true%false,type(true%false)) shows error
+print("Exponentiation:",true**false,type(true**false))
+
+#Task 14
+exp_1=True + 5
+exp_2=False + 5
+exp_3=True * 10
+exp_4=False * 10
+exp_5=True - 5
+exp_6=False - 5
+print(exp_1)
+print(exp_2)
+print(exp_3)
+print(exp_4)
+print(exp_5)
+print(exp_6)
+
+
+#Task 15
+str1="Daksh"
+str2="Khandelwal"
+str3=str1+str2
+print(str3)
+
+#Task 16
+str_4="Python"
+print(str_4*3)
+# print(str2*2.5) Shows TypeError
+
+#Task 17
+print(str1+str2)
+# print(str1-str2) shows error
+print(str1*2)
+# print(str2/str1) shows error
+
+#Task 18
+value=None
+#print("Addition: ",None+2)
+#print("Subtraction: ",None-2)
+#print("Multiplication: ",None*2)
+#print("Division: ",None/2)
+#print("Floor Division: ",None//2)
+#print("Modulus: ",None%2)
+#print("Exponentiation: ",None**2)
+
+#Task 19
+#print(1/0)
+#print("Python"/0)
+#print(None/0)
+
+#Task 20
+no_1=4
+no_2=3
+print("Addition: ",no_1+no_2) #Addition
+print("Subtraction: ",no_1-no_2) #Subtraction
+print("Multiplication: ",no_1*no_2) #Multiplication
+print("Division: ",no_1/no_2) #Division
+print("Floor Division: ",no_1//no_2) #Fllor Division
+print("Modulus: ",no_1%no_2) #Modulus
+print("Exponentiation: ",no_1**no_2) #Exponentiation
+
+#Task 21
+a = 10
+b = -3
+c = 2.5
+
+print(a*b/c)
+print((a*b)/c)
+print(a+b*c**b)
+print((a+b)*c**b)
+print(a-c/b//a**c)
+print(c-a*b**a)
+print(b%a+c*b)
+print(a+b//c**a%b)
+print((a+b)//c**(a%b))
+print((a-c)*b**a(b+a))
+
+
+
+
+
+
+
+
+
+
