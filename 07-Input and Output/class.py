5b2b0553e1a50cb486f3815f6b904050ef1b00be
neel-parmar-21 <neel.parmar.cg@gmail.com>
2026-09-04 00:50:54 +0530

    Add Input and Output class practice

---
+++ 07-Input and Output/class.py
@@ -0,0 +1,29 @@
+name = input("Enter your name: ")
+lab = int(input("Enter your lab: "))
+product_name = input("Enter the product's name: ")
+quantity = int(input("Enter the quantity: "))
+price =float(input("Enter the price: "))
+total_price = quantity*price
+
+
+print(f"My name is: {name}""\n"
+      f"My lab is: {lab}""\n"
+      f"The product name is: {product_name}""\n"
+      f"The quantity is: {quantity}""\n"
+      f"The price is: {price}""\n"
+      f"The total price is: {total_price}""\n"
+)
+a,b=map(int,input("Enter two numbers: ").split())
+print(a,b, type(a), type(b))
+
+a,b=map(int,input("Enter two numbers: ").split()[:2])
+print(a,b, type(a), type(b),)
+
+
+
+
+
+
+
+
+
