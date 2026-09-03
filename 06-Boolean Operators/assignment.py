275499d3c02ee6a0467fee00e23b38cbfb14a185
neel-parmar-21 <neel.parmar.cg@gmail.com>
2026-09-04 00:49:02 +0530

    Add Boolean Operators assignment

---
+++ 06-Boolean Operators/assignment.py
@@ -0,0 +1,94 @@
+#Task 15
+age = 20
+print(age >= 18)
+
+#Task 16
+marks = 45
+print(marks == 50)
+
+#Task 17
+age = 18
+print(age >= 18)
+
+#Task 18
+print(True and True,
+      True and False,
+      False and True,
+      False and False
+)
+
+#Task 19
+print(True or True,
+      True or False,
+      False or True,
+      False or False
+)
+
+#Task 20
+print(not True,
+      not False)
+
+#Task 22
+age = 25
+print(age >= 18)
+print(age <= 60)
+print(age >= 18 and age <= 60)
+
+#Task 23
+age = 16
+print(age < 18 or age > 60)
+
+#Task 24
+age = 20
+print(not age < 18)
+
+#Task 25
+number=18
+print(number > 10 and number < 50)
+
+#Task 26
+number=19
+print(number < 10 and number > 100)
+
+#Task 27
+number=20
+print(not number > 12)
+
+#Task 28
+print(bool(0),
+      bool(1),
+      bool(-5),
+      bool(""),
+      bool("Python"),
+      bool(False),
+      bool(True),
+      bool(None)
+)
+
+#Task 29
+print(bool(0),
+      bool(10),
+      bool(""),
+      bool("Hello"),
+      bool(None),
+
+)
+
+#Task 30
+#Task 28
+print(type(0),
+      bool(0),
+      type(1),
+      bool(1),
+      type(""),
+      bool(""),
+      type("Python"),
+      bool("Python"),
+      type(False),
+      bool(False),
+      type(True),
+      bool(True),
+      type(None),
+      bool(None)
+)
+
