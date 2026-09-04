04d75a50fbd05186c2084e0ebd5ce84d6ea133fa
neel-parmar-21 <neel.parmar.cg@gmail.com>
2026-09-04 18:11:39 +0530

    Add Input and Output assignment

---
+++ 07-Input and Output/assignment.py
@@ -0,0 +1,165 @@
+#Task 1
+name = input("Enter your name: ")
+print(name)
+
+#Task 2
+city = input("Enter your city: ")
+print(f"Your city is {city}")
+
+#Task 3
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(name)
+print(age)
+
+#Task 5
+marks = input("Enter your marks: ")
+print(type(marks))
+
+#Task 6
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + last_name
+print(full_name)
+
+#Task 7
+name = input("Enter your name: ")
+city = input("Enter your city: ")
+college = input("Enter your college name: ")
+print(name)
+print(city)
+print(college)
+
+#Task 8
+first_name, last_name = input("Enter your first and last name: ". split())
+print(first_name)
+print(last_name)
+
+#Task 9 
+# it will receive "Python" and "Programming"
+
+#Task 10
+name, age, city = input("Enter your name, age and city: ". split())
+print(name)
+print(age)
+print(city)
+
+#Task 11
+a = int("25")
+print(type(a))
+
+#Task 12
+a = float("25.5")
+print(type(a))
+
+#Task 13
+a = str(100)
+print(type(a))
+
+#Task 14
+number = int(input("Enter a number: "))
+print(type(number))
+
+#Task 15
+number = float(input("Enter a decimal number: "))
+print(type(number))
+
+#Task 16
+# because python takes default string as input and that's why it produce string concatenation
+
+#Task 17
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(a + b)
+
+#Task 18
+name = "Rahul"
+age = 20
+
+print(f"My name is {name} and I am {age} years old.")
+
+#Task 19
+a = 10
+b = 20
+
+print(f"The total is {a+b}")
+
+#Task 20
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"My name is {name} and I am {age} years old")
+
+#Task 21
+price = float(input("Enter the price: "))
+print(f"The price is {price:.2f}")
+
+#Task 22
+# it rounds the floating point value to 2
+
+#Task 23
+product_name = input("Enter the product's name: ")
+price = float(input("Enter the product's price: "))
+quantity = int(input("Enter the quantity: "))
+
+print(f"Product name: {product_name} Price: {price} Quantity: {quantity}")
+
+#Task 24
+# A B C
+
+#Task 25
+print("2026", "08", "19", sep="-")
+
+#Task 26
+print("Hello", end=" ")
+print("World")
+
+#Task 27
+number_1 = input("Enter the first number: ")
+number_2 = input("Enter the second number: ")
+
+print(f"First number: {number_1}")
+print(f"Second number: {number_2}")
+print(f"Sum: {number_1 + number_2}")
+
+#Task 28
+price = input("Enter the price: ")
+quantity = input("Enter the quantity: ")
+total = price + quantity 
+
+print(f"Price: {price}")
+print(f"Quantity: {quantity}")
+print(f"Total: {total}")
+
+#Task 29
+student_name = input("Enter the student's name: ")
+age = int(input("Enter the student's age: "))
+marks = float(input("Enter the student's marks: "))
+
+print(f"Student's Name: {student_name} Age: {age} Marks: {marks}")
+
+#Task 30
+#Student Information
+student_name = input("Enter the student's name: ")
+age = int(input("Enter the student's age: "))
+height = float(input("Enter the student's height: "))
+city = input("Enter your city name: ")
+
+print(f"Student's Name: {student_name} Age: {age} Height: {height} City: {city}")
+print(f"Height: {height:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
