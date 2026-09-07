526ac50a1ea583ee390c9c532ac72796cdafbff9
neel-parmar-21 <neel.parmar.cg@gmail.com>
2026-09-07 11:28:14 +0530

    Add conditional statements class practice

---
+++ 08-Conditional Statements/class.py
@@ -0,0 +1,29 @@
+# username = input("Enter your username: ")
+# password = input("Enter your password: ")
+# age = int(input("Enter your age: "))
+# terms= input("Do you accept the terms and conditions: ")
+
+# if username=="Neel":
+#     if password=="python123":
+#         if age>=18:
+#             if terms!="yes":
+#                 print("Accept the terms and conditions first!!")
+#                 if terms=="yes":
+#                     print("Login Successful! Welcome!")
+
+# print("18"==18)
+# print("18">=18)
+
+is_indian = input("Are you Indian? YES or No: ")
+
+if is_indian == "Yes":
+    print("Welcome to our team!")
+    
+else:
+    if is_indian == "No":
+        print("You're not selected")
+    else:
+        print("Enter a valid value")
+
+
+
