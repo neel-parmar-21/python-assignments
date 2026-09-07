9a860f92449479e721eb8c46006486844ae790cb
neel-parmar-21 <neel.parmar.cg@gmail.com>
2026-09-08 00:04:30 +0530

    Removed unnecessary comments in class practice

--- 08-Conditional Statements/class.py
+++ 08-Conditional Statements/class.py
@@ -1,46 +1,46 @@
-# username = input("Enter your username: ")
-# password = input("Enter your password: ")
-# age = int(input("Enter your age: "))
-# terms= input("Do you accept the terms and conditions: ")
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+age = int(input("Enter your age: "))
+terms= input("Do you accept the terms and conditions: ")
 
-# if username=="Neel":
-#     if password=="python123":
-#         if age>=18:
-#             if terms!="yes":
-#                 print("Accept the terms and conditions first!!")
-#                 if terms=="yes":
-#                     print("Login Successful! Welcome!")
+if username=="Neel":
+    if password=="python123":
+        if age>=18:
+            if terms!="yes":
+                print("Accept the terms and conditions first!!")
+                if terms=="yes":
+                    print("Login Successful! Welcome!")
 
-# print("18"==18)
-# print("18">=18)
+print("18"==18)
+print("18">=18)
 
-# is_indian = input("Are you Indian? YES or No: ")
+is_indian = input("Are you Indian? YES or No: ")
 
-# if is_indian == "Yes":
-#     print("Welcome to our team!")
+if is_indian == "Yes":
+    print("Welcome to our team!")
     
-# else:
-#     if is_indian == "No":
-#         print("You're not selected")
-#     else:
-#         print("Enter a valid value")
+else:
+    if is_indian == "No":
+        print("You're not selected")
+    else:
+        print("Enter a valid value")
 
-# marks = int(input("Enter your marks: "))
+marks = int(input("Enter your marks: "))
 
-# if marks > 89:
-#     print("A")
+if marks > 89:
+    print("A")
 
-# elif marks > 74:
-#     print("B")
+elif marks > 74:
+    print("B")
 
-# elif marks > 59:
-#     print("C")
+elif marks > 59:
+    print("C")
 
-# elif marks > 39:
-#     print("D")
+elif marks > 39:
+    print("D")
 
-# else:
-#     print("F")
+else:
+    print("F")
 
 print("1 = Addition""\n"
       "2 = Subtraction""\n"
