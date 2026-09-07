7a295a81b0f2079191cad36bcf99c585cd441c6a
neel-parmar-21 <neel.parmar.cg@gmail.com>
2026-09-08 00:01:55 +0530

    Update conditional statements practice

---
+++ 08-Conditional Statements/assignment.py
@@ -0,0 +1,38 @@
+#Task 1
+number = input("Enter a number: ")
+if number > 10:
+    print("Greater than 10")
+
+#Task 2
+age = input("Enter your age: ")
+if age >= 18:
+    print("Adult")
+
+#Task 3
+number = input("Enter a number: ")
+if number > 0:
+    print("Positive")
+
+#Task 4
+marks = input("Enter your marks: ")
+if marks >= 40:
+    print("Pass")
+
+#Task 5
+number = input("Enter a number: ")
+if number == 0:
+    print("Zero")
+
+#Task 6
+number = input("Enter a number: ")
+if number >= 0:
+    print("Positive")
+else:
+    print("Not positive")
+
+#Task 7
+age = input("Enter your age: ")
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
--- 08-Conditional Statements/class.py
+++ 08-Conditional Statements/class.py
@@ -14,16 +14,66 @@
 # print("18"==18)
 # print("18">=18)
 
-is_indian = input("Are you Indian? YES or No: ")
+# is_indian = input("Are you Indian? YES or No: ")
 
-if is_indian == "Yes":
-    print("Welcome to our team!")
+# if is_indian == "Yes":
+#     print("Welcome to our team!")
     
+# else:
+#     if is_indian == "No":
+#         print("You're not selected")
+#     else:
+#         print("Enter a valid value")
+
+# marks = int(input("Enter your marks: "))
+
+# if marks > 89:
+#     print("A")
+
+# elif marks > 74:
+#     print("B")
+
+# elif marks > 59:
+#     print("C")
+
+# elif marks > 39:
+#     print("D")
+
+# else:
+#     print("F")
+
+print("1 = Addition""\n"
+      "2 = Subtraction""\n"
+      "3 = Multiplication""\n"
+      "4 = Division"
+       )
+
+operation = int(input("Enter the mode of operation you want to perform: "))
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+if operation == 1:
+    print(f"Addition: {number1 + number2}")
+
+elif operation == 2:
+    print(f"Subtraction: {number1 - number2}")
+
+elif operation == 3:
+    print(f"Multiplication: {number1 * number2}")
+
+elif operation == 4:
+    print(f"Division: {number1 / number2}")
+
 else:
-    if is_indian == "No":
-        print("You're not selected")
-    else:
-        print("Enter a valid value")
+    print("Enter a valid operation number")
+
+
+
+
+
+
+
+
 
 
 
